2f559eed0a9097c1acb193328280aa9802b2418d
Hilquias Ferreira Melo <hilquiasfmelo@gmail.com>
2025-04-19 18:10:23 -0300

    criando form e funcoes junto com estados

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -8,33 +8,55 @@ import {
   TableHeader,
   TableRow,
 } from '@/components/ui/table'
-import { Link as Link2, Search } from 'lucide-react'
+import { ChevronDown, Link as Link2, Search } from 'lucide-react'
+import { useState } from 'react'
 import { Helmet } from 'react-helmet-async'
 import { Link } from 'react-router'
 
 export function Home() {
+  const [nameCrypton, setNameCrypton] = useState('')
+
+  function handleSearchCrypto(e: React.FormEvent<HTMLFormElement>) {
+    e.preventDefault()
+
+    console.log(nameCrypton)
+  }
+
+  function handleGetMoreCryptons(e: React.MouseEvent<HTMLButtonElement>) {
+    e.preventDefault()
+
+    console.log('get more cryptons')
+  }
+
   return (
     <>
       <Helmet title="Home" />
 
-      <div className="flex items-center gap-2 max-w-5xl mx-auto">
+      <form
+        className="flex items-center gap-2 max-w-5xl mx-auto"
+        onSubmit={handleSearchCrypto}
+      >
         <Input
           className="rounded focus-visible:ring-1 focus-visible:ring-offset-0 placeholder:text-muted-foreground/50 placeholder:text-sm"
           placeholder="Digite o nome da criptomoeda"
+          value={nameCrypton}
+          onChange={e => setNameCrypton(e.target.value)}
         />
-        <Button className="rounded">
+        <Button type="submit" className="rounded">
           <Search />
         </Button>
-      </div>
+      </form>
 
-      <div className="max-w-5xl mx-auto mt-8 border rounded overflow-hidden overflow-x-auto">
+      <div className="max-w-5xl mx-auto mt-8 border rounded overflow-x-auto">
         <Table>
           <TableHeader className="border-b bg-muted/40 whitespace-nowrap">
-            <TableHead>Moeda</TableHead>
-            <TableHead>Valor de mercado</TableHead>
-            <TableHead>Preço</TableHead>
-            <TableHead>Volume</TableHead>
-            <TableHead>Variação 24h</TableHead>
+            <TableRow>
+              <TableHead>Moeda</TableHead>
+              <TableHead>Valor de mercado</TableHead>
+              <TableHead>Preço</TableHead>
+              <TableHead>Volume</TableHead>
+              <TableHead>Variação 24h</TableHead>
+            </TableRow>
           </TableHeader>
 
           <TableBody>
@@ -53,12 +75,21 @@ export function Home() {
                 <TableCell>USD</TableCell>
                 <TableCell>1.000.000</TableCell>
                 <TableCell>1.000.000</TableCell>
-                <TableCell>1.000.000</TableCell>
+                <TableCell className="text-green-500">1.000.000</TableCell>
               </TableRow>
             ))}
           </TableBody>
         </Table>
       </div>
+
+      <Button
+        type="button"
+        variant="outline"
+        onClick={handleGetMoreCryptons}
+        className="mt-4 rounded font-semibold"
+      >
+        <span className="mb-1">Carregar mais</span> <ChevronDown />
+      </Button>
     </>
   )
 }
